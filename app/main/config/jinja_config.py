3bd840ebcd60f4fe340e07743d2413d392106ed6
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-09-11 14:14:08 +0100

    :recycle: Change the templates to show a nice welcome

--- app/main/config/jinja_config.py
+++ app/main/config/jinja_config.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from jinja2 import ChoiceLoader, PackageLoader, PrefixLoader
 
-from app.main.config.app_config import app_config
-
 
 def configure_jinja(app: Flask) -> None:
     app.jinja_loader = ChoiceLoader(
@@ -15,4 +13,3 @@ def configure_jinja(app: Flask) -> None:
     )
     app.jinja_env.trim_blocks = True
     app.jinja_env.lstrip_blocks = True
-    app.jinja_env.globals["phase_banner_text"] = app_config.phase_banner_text
